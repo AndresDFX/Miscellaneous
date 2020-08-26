9b4d8d00148690fb06e6eadb111564a7a04089bf
AndresDFX <andres_dfx@hotmail.com>
2020-08-26 01:42:20 -0500

    Add OpenGL (C++)

---
+++ C++/OpenGL/Compilar.bat
@@ -0,0 +1,18 @@
+@echo off
+::Borrar archivos de alguna compilacion anterior
+DEL /F /Q window.o window.exe
+
+::Compilar el archivo.o y crear .exe
+for %%i in (*.cpp) do (
+	g++ -c -o window.o %%i -I"C:\MinGW\GLUT\include" 
+	echo Compilacion completa
+	
+	g++ -o window.exe window.o -L"C:\MinGW\GLUT\lib" -lglut32 -lopengl32 -Wl,--subsystem,windows	
+	echo Ejecutable creado correctamente
+	)
+
+::Ejecutar el programa
+echo Ejecutando el programa
+window.exe
+exit
+
